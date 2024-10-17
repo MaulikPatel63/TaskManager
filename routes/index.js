9751e4c581ac93968e46ebcf1e3f9491daf02957
MaulikPatel63 <maulik.paghadal3301@gmail.com>
2024-10-17 06:05:20 +0530

    2024-10-17 upload 2

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const protectRoute = require("../middleware/protectRoute.js");
 
 //! Auth Router
-router.use("/api/v1/auth", require("./Auth"));
+router.use("/api/v1/auth", require("./Auth.js"));
 
 //! Task Router
-router.use("/api/v1/task", protectRoute, require("./Task"));
+router.use("/api/v1/task", protectRoute, require("./Task.js"));
 
 //! Category Router
-router.use("/api/v1/category", protectRoute, require("./Category"));
+router.use("/api/v1/category", protectRoute, require("./Category.js"));
 
 //! Admin Task Router
-router.use("/api/v1/admintask", protectRoute, require("./AdminTask"));
+router.use("/api/v1/admintask", protectRoute, require("./AdminTask.js"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const app = require("./app");
+const app = require("./src/app");
 const { connectDB } = require("./config/db");
 const ENV_VARS = require("./config/envVars");
 
